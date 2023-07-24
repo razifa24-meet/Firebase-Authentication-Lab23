82fbd25a9a1c4a76ae8be59c83389d1a9a8b366e
razifa24-meet <139681113+razifa24-meet@users.noreply.github.com>
2023-07-24 17:46:44 +0300

    DONE FINAL LAB

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -7,12 +7,13 @@ config = {"apiKey": "AIzaSyC80DIkcnPCQXaArh3v8WvT68oIAKABQ1k",
   "projectId": "zawardo-49c1e",
   "storageBucket": "zawardo-49c1e.appspot.com",
   "messagingSenderId": "58476508356",
-  "appId": "1:58476508356:web:5c2547be17d2a044ba4591", "databaseURL": "" }
+  "appId": "1:58476508356:web:5c2547be17d2a044ba4591", "databaseURL": "https://last-cs-lecture-default-rtdb.europe-west1.firebasedatabase.app/" }
 
 
 
 firebase = pyrebase.initialize_app(config)
 auth= firebase.auth()
+db = firebase.database()
 
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
@@ -42,19 +43,56 @@ def signup():
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
+        username = request.form['username']
+        bio = request.form['bio']
+        full_name = request.form['fullname']
+        
         try:
             login_session['user'] = auth.create_user_with_email_and_password(email, password)
-            return redirect(url_for('signin'))
+            UID = login_session['user']['localId']
+            user = {"name": full_name, "email": email, "username" : username, "bio" : bio}
+            db.child("Users").child(UID).update(user)
+            return redirect(url_for('add_tweet'))
+
         except:
             error = "Authentication failed"
+
     return render_template("signup.html")
 
 
 
+@app.route('/signout')
+def signout():
+    auth.current_user = None
+    login_session['user'] = None
+    return redirect(url_for('signin'))
 
 @app.route('/add_tweet', methods=['GET', 'POST'])
 def add_tweet():
+    if request.method == 'POST':
+        title = request.form['title']
+        text = request.form['text']
+        try:
+            UID = login_session['user']['localId']
+            tweet = {'title':title, 'text':text, 'UID': UID}
+            db.child("Tweets").push(tweet)
+            return redirect(url_for('all_tweets'))
+        except:
+            error = "your dumb"
+            
+
     return render_template("add_tweet.html")
+
+
+@app.route('/all_tweets')
+def all_tweets():
+    
+    UID = login_session['user']['localId']
+
+    dici = db.child("Tweets").get().val()
+
+
+    return render_template('tweet.html', tweet123 = dici)
 
 
 if __name__ == '__main__':
